b0163bf116934d08c83e84dd5db0d136f26bfa92
Marek Szweda <mszweda@kbj.com.pl>
2020-07-14 14:13:54 +0200

    Dodanie routingu.

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express'); // import frameworka
 const app     = express();          // tworzę nową aplikację
 
+const path    = require('path');
+const router  = express.Router();
+
 // co sie stanie po wejściu na główny adres
-app.get('/', function (req, res) {
-    res.json(
-        {'status': 'Success'}
-    );
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
 });
 
+router.get('/home', function (req, res) {
+    res.json(
+        {'status' : 'home'}
+    )
+});
+
+app.use('/', router);
 app.listen(8081, function () {
     console.log('Server started');
 });
